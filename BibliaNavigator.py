babb4f7ad928ab20690877e2b2dce4d7e6df552f
emc <emc>
2024-06-12 03:57:43 +0200

    implemented recognition of different languages. No empty lines.

--- BibliaNavigator.py
+++ BibliaNavigator.py
@@ -4,79 +4,37 @@ from bs4 import BeautifulSoup
 import requests
 import re
 from PIL import Image, ImageTk
-
-
-def get_text_content(content):
-    text = ""
-    for i in content:
-        if i.string is not None and i.string != ' ':
-            if len(text) > 0:
-                if text[-1] == '#':
-                    text += i.string + "#\n"
-                elif text[-1] == '\n':
-                    text += i.string
-                else:
-                    text += "\n" + i.string
-            else:
-                text += i.string
-    return text
-
-
-def convert_to_swiss_german_letters(text):
-    return text.replace('ß', 'ss')
-
-
-def remove_hashtag_comments(text):
-    return re.sub("#.*#", "", text)
-
-def remove_subtitle():
-    pass
-
-def starts_with_numbers(text):
-    return text.isnumeric()
-
-
-def convert_to_dictionary(text):
-    text = text.split("\n")
-    while "" in text:
-        text.remove("")
-    dictionary = dict()
-    number = -1
-    content = ""
-    for line in text:
-        if starts_with_numbers(line):
-            number = int(line) - 1
-            if content != "":
-                dictionary[number] = content
-                content = ""
-        else:
-            content += line
-    dictionary[number] = content
-    return dictionary
-
+from capiteltext import CapitelText
 
 image = Image.open('python_img.jpeg')
 image = image.resize((500, 300))
 
 # http = urllib3.PoolManager()
-url = 'https://www.bible.com/de/bible/73/GEN.1.HFA'
-#url = "https://www.bible.com/de/bible/58/GEN.1.ELB71"
-# url = "https://www.bible.com/de/bible/149/GEN.1.RVR1960"
-url = "https://www.bible.com/de/bible/149/GEN.2.RVR1960"
-# url = "https://www.bible.com/de/bible/149/GEN.3.RVR1960"
-url = "https://www.bible.com/de/bible/149/GEN.9.RVR1960"
-webpage = requests.get(url, 'html.parser')
-#response = http.request('GET', url)
-soup = BeautifulSoup(webpage.content, features="html.parser")
 
-result = soup.findAll("span")
-print(result)
-text = convert_to_swiss_german_letters(get_text_content(result))
-# print(text)
-text = remove_hashtag_comments(text)
-# print(text)
-dictionary = convert_to_dictionary(text)
-# print(dictionary)
+url1 = 'https://www.bible.com/de/bible/73/GEN.1.HFA'
+url2 = "https://www.bible.com/de/bible/58/GEN.1.ELB71"
+url3 = "https://www.bible.com/de/bible/149/GEN.1.RVR1960"
+url4 = "https://www.bible.com/de/bible/149/GEN.2.RVR1960"
+url5 = "https://www.bible.com/de/bible/149/GEN.3.RVR1960"
+url6 = "https://www.bible.com/de/bible/149/GEN.9.RVR1960"
+url7 = "https://www.bible.com/de/bible/149/2SA.12.RVR1960"
+urls = [url1, url2, url3, url4, url5, url6, url7]
+
+capitel = None
+for url in urls:
+    webpage = requests.get(url, 'html.parser')
+    capitel = CapitelText(webpage)
+    print(capitel.whole_title)
+    print(capitel.text)
+# print(webpage.content)
+### response = http.request('GET', url)
+#soup = BeautifulSoup(webpage.content, features="html.parser")
+#result = soup.findAll("span")
+
+# capitel = CapitelText(webpage)
+# capitel.get_title()
+
+result = capitel.text
 
 ## Set the widget (GUI)
 root = tk.Tk()
@@ -85,15 +43,15 @@ root.geometry('1280x800')
 title = tk.Label(root, text="BibliaNavigator", bg="red", fg="white")
 title.pack()
 # label = tk.Label(root, image=img)
-#lblCapitel = tk.Label(root, text=text, wraplength=500)
+# lblCapitel = tk.Label(root, text=text, wraplength=500)
 scroll_bar = tk.Scrollbar(root, orient="vertical")
 scroll_bar.pack(side=tk.RIGHT, fill=tk.Y)
 text_widget = tk.Text(root, font=("Arial", 30), yscrollcommand=scroll_bar.set)
 text_widget.pack(side=tk.LEFT)
-text_widget.insert(tk.END, text)
+text_widget.insert(tk.END, result)
 scroll_bar.config(command=text_widget.yview)
 # label.pack()
-#lblCapitel.pack()
+# lblCapitel.pack()
 
 root.title('BibliaNavigator by M. Fässler')
 
---
+++ capiteltext.py
@@ -0,0 +1,121 @@
+from bs4 import BeautifulSoup
+import re
+
+
+class CapitelText:
+    def __init__(self, webpage):
+        self.webpage = webpage
+        self.whole_title = ""
+        self.book_title = ""
+        self.book_number = 0
+        self.capitel_number = 0
+        self.text = ""
+        self.dictionary = dict()
+        self.soup = BeautifulSoup(self.webpage.content, 'html.parser')
+        # print(self.soup)
+        self.text = self.soup.findAll("span")
+        self.get_text_content_new()
+        self.text = self.remove_hashtag_comments(self.text)
+        self.put_brackets_in_same_line()
+        self.remove_double_lines()
+        self.remove_empty_lines()
+        self.convert_to_dictionary()
+        self.whole_title = self.get_capitel_title()
+        #print(self.text)
+
+    def get_text_content_new(self):
+        text = ""
+        lastLine = ""
+        for line in self.text:
+            # ChapterContent_body__O3qjr
+            if line['class'] == ['ChapterContent_body__O3qjr']:
+                text += "<"
+                for i in line:
+                    if i.string != None:
+                        lastLine += i.string + "\n"
+                        text += i.string
+                text += ">\n"
+            else:
+                if line.string != None:
+                    if line.string not in lastLine:
+                        text += line.string + "\n"
+                        lastLine = ""
+        self.text = text
+
+    def put_brackets_in_same_line(self):
+        while re.search(r"\(\s*.*\s.*\s\)", self.text) != None:
+            match = re.search(r"\(\s*.*\s.*\s\)", self.text)
+            text = match.group()
+            text = text.replace("\n", "")
+            self.text = re.sub(r"\(\s*.*\s.*\s\)", text, self.text)
+
+    def get_text_content(self, content):
+        text = ""
+        for i in content:
+            if i.string is not None and i.string != ' ':
+                if len(text) > 0:
+                    if text[-1] == '#':
+                        text += i.string + "#\n"
+                    elif text[-1] == '\n':
+                        text += i.string
+                    else:
+                        text += "\n" + i.string
+                else:
+                    text += i.string
+        return text
+
+    def get_capitel_title(self):
+        text = self.soup.find_all("h1")
+        return text[0].string
+
+    #TODO implement get capitel title, book number and book title
+    def get_book_meta_data(self):
+        pass
+
+    def convert_to_swiss_german_letters(self, text):
+        return text.replace('ß', 'ss')
+
+    def remove_hashtag_comments(self, text):
+        return re.sub("#.*", "", text)
+
+    # TODO is it needed ??? removing text explanations
+    def remove_subtitle(self):
+        pass
+
+    def starts_with_numbers(self, text):
+        return text.isnumeric()
+
+    # TODO implement method
+    def convert_to_dictionary(self):
+        text = self.text.split("\n")
+        while "" in text:
+            text.remove("")
+        dictionary = dict()
+        number = -1
+        content = ""
+        for line in text:
+            if self.starts_with_numbers(line):
+                number = int(line) - 1
+                if content != "":
+                    dictionary[number] = content
+                    content = ""
+            else:
+                content += line
+        dictionary[number] = content
+        self.dictionary = dictionary
+
+    def remove_double_lines(self):
+        newText = ""
+        lastLine = ""
+        for line in self.text.splitlines():
+            if lastLine != line:
+                newText += line + "\n"
+            lastLine = line
+        self.text = newText
+
+    def remove_empty_lines(self):
+        newText = ""
+        for line in self.text.splitlines():
+            if line != " " and line != "\n" and line != "":
+                newText += line + "\n"
+        self.text = newText
